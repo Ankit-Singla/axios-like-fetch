3096f59ec1465cc07647512629b68e64a9097b13
Ankit Singla <asingla590@gmail.com>
2020-11-29 18:34:23 +0530

    Cancellation and timeout

--- axiosLikeFetch.js
+++ axiosLikeFetch.js
@@ -1,9 +1,18 @@
 import 'isomorphic-unfetch';
+import AbortController from 'abort-controller';
 
 const axiosLikeFetch = (config) => {
     return captainFetch(config)
         .then(data => ({...data, config}));
 };
+
+export class CancelToken {
+    constructor(executor) {
+        const controller = new AbortController();
+        executor(() => controller.abort());
+        return controller;
+    }
+}
 
 const transformRes = (res) => {
     return res.text().then(text => {
@@ -18,14 +27,19 @@ const captainFetch = (config) => {
     const {
         url,
         transformResponse=transformRes,
+        timeout=0,
+        cancelToken=new AbortController(),
     } = config;
     delete config.url;
 
-    return fetch(url, config)
+    if(timeout) {
+        setTimeout(() => {
+            controller.abort();
+        }, timeout);
+    }
+    return fetch(url, {...config, signal: cancelToken.signal})
         .then(res =>  transformResponse(res)
         .then(body => ({status: res.status, statusText: res.statusText, headers: res.headers, body})));
 };
-
-
 
 export default axiosLikeFetch;
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // for testing
-import axiosLikeFetch from './axiosLikeFetch.js';
+import axiosLikeFetch, { CancelToken } from './axiosLikeFetch.js';
 
 const url = 'https://cat-fact.herokuapp.com/facts/random';
-axiosLikeFetch({ url })
+const transformResponse = (res) => {
+    return res.blob().then(blob => {
+        return blob;
+    });
+};
+let cancel;
+axiosLikeFetch({ url, cancelToken: new CancelToken((param) => {cancel = param}) })
 .then(data => console.log(data));
+
+// cancel();
