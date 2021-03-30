e200e09dbd40bf2e1e54fc5d69ed9ced895882af
Aryan-Abhishek <arya2001abhishek@gmail.com>
2021-03-30 20:47:07 +0530

    working master

--- index.js
+++ index.js
@@ -1,70 +1,35 @@
 // for testing
-import axiosLikeFetch, { CancelToken } from "./axiosLikeFetch.js";
+import axiosLikeFetch, { CancelToken } from './axiosLikeFetch.js';
 
-// const url = "https://cat-fact.herokuapp.com/facts/random";
-
-const url = "https://jsonplaceholder.typicode.com/todos";
-
-const transformResponse = [
-  (data) => {
+const url = 'https://cat-fact.herokuapp.com/facts/random';
+const transformResponse = [(data) => {
     // return res.blob().then(blob => {
     //     return blob;
     // });
-    return (data = JSON.parse(data || {}));
-  },
-  (data) => {
-    return { ...data, name: "history" };
-  },
-];
-
-const transformRequest = [
-  (data, header) => {
-    return { data, header };
-  },
-  (data, header) => {
-    return { data, header };
-  },
-];
-
+    return data;
+}];
 // let cancel;
 // axiosLikeFetch({ url, cancelToken: new CancelToken((param) => {cancel = param}) })
 // .then(data => console.log(data));
 // // cancel();
 
 axiosLikeFetch.request.interceptors.use((config) => {
-  if (!config.headers) config.headers = [];
-  config.headers["Content-Type"] = "application/json";
-  return config;
+    if(!config.headers)
+        config.headers = [];
+    config.headers['Content-Type'] = 'application/json';
+    return config;
 });
 
 axiosLikeFetch.response.interceptors.use((res) => {
-  res.ok = true;
-  return res;
+    res.ok = true;
+    return res;
 });
 
 const controller = new axiosLikeFetch.AbortController();
 const signal = controller.signal;
-signal.addEventListener("abort", () => {
-  console.log("Request Aborted");
+signal.addEventListener('abort', () => {
+    console.log('Request Aborted');
 });
-
-// transform Res
-// axiosLikeFetch({
-//   url,
-//   cancelToken: controller,
-//   params: { name: "ankit", surname: "singla" },
-//   transformResponse,
-// }).then((data) => console.log(data));
+axiosLikeFetch({ url, cancelToken: controller, params: {name: "ankit", surname: "singla"} })
+.then(data => console.log(data));
 // controller.abort();
-
-// transform Req
-let toDoDetails = {
-  firstName: "Finn",
-  lastName: "Williams",
-};
-
-axiosLikeFetch({
-  url,
-  method: "POST",
-  data: toDoDetails,
-}).then((data) => console.log(data));
